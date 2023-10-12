588ce19ea12c6bb8ca4439251dfdded250aeddca
vladislav-slobodenjuk <vladjun@gmail.com>
2023-10-12 23:33:22 +0300

    updt taskSchema for Task model; updt taskAddSchema for Joi

--- models/task.js
+++ models/task.js
@@ -2,40 +2,43 @@ import { Schema, model } from "mongoose";
 import Joi from "joi";
 import { hookError } from "./hooks.js";
 
-const timePattern = /^[0-2][0-3]:[0-5][0-9]$/;
-const datePattern = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+const timePattern = /^[0-2][0-3]:[0-5][0-9]$/; // /^[00-23]:[00-59]$/
+const datePattern = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/; // може це номер?
+
+const categoryList = ["to-do", "in-progress", "done"];
+const priorityList = ["low", "medium", "high"];
 
 const taskSchema = new Schema(
   {
     title: {
       type: String,
       maxlength: 250,
-      required: [true, "Title name for contact"],
+      required: [true, "Title is required"],
     },
     start: {
       type: String,
       match: timePattern,
-      required: [true, "Start is required"],
+      required: [true, "Start time is required"],
     },
     end: {
       type: String,
       match: timePattern,
-      required: [true, "End is required"],
+      required: [true, "End time is required"],
     },
     priority: {
       type: String,
-      enum: ["low", "medium", "high"],
+      enum: priorityList,
       required: [true, "Priority is required"],
     },
     date: {
       type: String,
       match: datePattern,
-      required: [true, "Date name for contact"],
+      required: [true, "Date is required"],
     },
     category: {
       type: String,
-      enum: ["to-do", "in-progress", "done"],
-      required: [true, "Category name for contact"],
+      enum: categoryList,
+      required: [true, "Category is required"],
     },
     owner: {
       type: Schema.Types.ObjectId,
@@ -47,7 +50,7 @@ const taskSchema = new Schema(
 
 taskSchema.post("save", hookError);
 
-const Task = model("task", taskSchema);
+export const Task = model("task", taskSchema);
 
 export const taskAddSchema = Joi.object({
   title: Joi.string()
@@ -64,6 +67,7 @@ export const taskAddSchema = Joi.object({
     .messages({ "any.required": `"end" mast be exist` }),
   priority: Joi.string()
     .required()
+    .valid(...priorityList)
     .messages({ "any.required": `"priority" mast be exist` }),
   date: Joi.string()
     .required()
@@ -71,7 +75,6 @@ export const taskAddSchema = Joi.object({
     .messages({ "any.required": `"date" mast be exist` }),
   category: Joi.string()
     .required()
+    .valid(...categoryList)
     .messages({ "any.required": `"category" mast be exist` }),
 });
-
-export default Task;
