546161137a99d6845b1e7c967ee082c99a0ccbac
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-10-14 13:12:43 +0200

    update user-model and user-validates

--- middleware/validation/validation.js
+++ middleware/validation/validation.js
@@ -1,9 +1,12 @@
-import { userSingSchema, userVerifySchema } from "../../models/user.js";
+import { authLoginSchema, authRegisterSchema, userVerifySchema } from "../../models/user.js";
 import { reviewAddSchema } from "../../models/review.js";
 import { taskAddSchema } from "../../models/Task.js";
 import { validateBody } from "../../decorators/index.js";
 
-export const ausValidate = validateBody(userSingSchema);
+export const authRegisterValidate = validateBody(authRegisterSchema);
+export const authLoginValidate = validateBody(authLoginSchema);
+
+// export const ausValidate = validateBody(userSingSchema);
 export const ausVerify = validateBody(userVerifySchema);
 export const reviewValidate = validateBody(reviewAddSchema);
 export const validateTaskAdd = validateBody(taskAddSchema);
--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -1,42 +1,25 @@
 import express from "express";
-import {
-  ausValidate,
-  ausVerify,
-} from "../../middleware/validation/validation.js";
+import { ausVerify, authLoginValidate, authRegisterValidate } from "../../middleware/validation/validation.js";
 import authenticate from "../../middleware/validation/authenticate.js";
 import ausController from "../../controllers/user-controller.js";
 import { upload } from "../../middleware/validation/upload.js";
 
 const authRouter = express.Router();
 
-authRouter.post(
-  "/register",
-  upload.single("avatar"),
-  ausValidate,
-  ausController.userReg
-);
+authRouter.post("/register", upload.single("avatar"), authRegisterValidate, ausController.userReg);
 
 authRouter.get("/verify/:verificationToken", ausController.getVerification);
 
 authRouter.post("/verify", ausVerify, ausController.repeatVerify);
 
-authRouter.post("/login", ausValidate, ausController.userLog);
+authRouter.post("/login", authLoginValidate, ausController.userLog);
 
-authRouter.patch(
-  "/avatars",
-  authenticate,
-  upload.single("avatar"),
-  ausController.userChangeAvatar
-);
+authRouter.patch("/avatars", authenticate, upload.single("avatar"), ausController.userChangeAvatar);
 
 authRouter.post("/logout", authenticate, ausController.logOut);
 
 authRouter.get("/current", authenticate, ausController.getCurrent);
 
-authRouter.patch(
-  "/subscription/:type",
-  authenticate,
-  ausController.changeSubscript
-);
+authRouter.patch("/subscription/:type", authenticate, ausController.changeSubscript);
 
 export default authRouter;
