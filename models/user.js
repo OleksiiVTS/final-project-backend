e6ac0e2beba17770b77ae197c219cb274948eb00
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-10-17 11:16:19 +0200

    update: user-model | removed invalid values, made the model and circuits more responsive

--- models/user.js
+++ models/user.js
@@ -5,10 +5,12 @@ import { getCurrentDate } from "../helpers/index.js";
 
 const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 const phonePattern = /^\+380\d{9}$/;
-const datePattern = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
-const withoutSpacesPattern = /^\S(.*\S)?$/;
+const datePattern =
+  /^(202[3-9]|20[3-9][0-9])-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
 
 const userBirthday = getCurrentDate();
+
+const themeList = ["dark", "light"];
 
 const userSchema = new Schema(
   {
@@ -49,7 +51,7 @@ const userSchema = new Schema(
     },
     theme: {
       type: String,
-      enum: ["dark", "light"],
+      enum: themeList,
       default: "light",
     },
     public_id: {
@@ -65,60 +67,93 @@ const userSchema = new Schema(
     },
     verificationToken: {
       type: String,
-      required: [true, "Verify token is required"],
+      required: [true, "Verification token is required"],
     },
   },
   { versionKey: false, timestamps: true }
 );
 
 userSchema.post("save", hookError);
-// userSchema.pre("findOneAndUpdate", runValidateAtUpdate);
-// userSchema.post("findOneAndUpdate", hookError);
 
 const User = model("user", userSchema);
 
 export const authRegisterSchema = Joi.object({
-  username: Joi.string()
-    .min(4)
-    .required()
-    .messages({ "any.required": `"username" mast be exist` }),
-  email: Joi.string()
-    .pattern(emailPattern)
-    .required()
-    .messages({ "any.required": `"email" mast be exist` }),
-  password: Joi.string()
-    .min(8)
-    .required()
-    .messages({ "any.required": `"password" mast be exist` }),
+  username: Joi.string().min(4).required().empty(false).messages({
+    "string.base": "The username must be a string.",
+    "any.required": "The username field is required.",
+    "string.empty": "The username must not be empty.",
+  }),
+  email: Joi.string().pattern(emailPattern).required().empty(false).messages({
+    "string.base": "The email must be a string.",
+    "any.required": "The email field is required.",
+    "string.empty": "The email must not be empty.",
+    "string.pattern.base": "The email must be in format test@gmail.com.",
+  }),
+  password: Joi.string().min(8).required().empty(false).messages({
+    "string.base": "The password must be a string.",
+    "any.required": "The password field is required.",
+    "string.empty": "The password must not be empty.",
+  }),
 });
 
 export const authLoginSchema = Joi.object({
-  email: Joi.string()
-    .pattern(emailPattern)
-    .required()
-    .messages({ "any.required": `"email" mast be exist` }),
-  password: Joi.string()
-    .min(8)
-    .required()
-    .messages({ "any.required": `"password" mast be exist` }),
+  email: Joi.string().pattern(emailPattern).required().empty(false).messages({
+    "string.base": "The email must be a string.",
+    "any.required": "The email field is required.",
+    "string.empty": "The email must not be empty.",
+    "string.pattern.base": "The email must be in format test@gmail.com.",
+  }),
+  password: Joi.string().min(8).required().empty(false).messages({
+    "string.base": "The password must be a string.",
+    "any.required": "The password field is required.",
+    "string.empty": "The password must not be empty.",
+  }),
 });
 
 export const authUpdateSchema = Joi.object({
-  username: Joi.string().min(4).pattern(withoutSpacesPattern),
-  birthday: Joi.string().pattern(datePattern),
-  email: Joi.string().pattern(emailPattern),
-  phone: Joi.string().pattern(phonePattern),
-  skype: Joi.string().pattern(phonePattern),
-  theme: Joi.string().valid("dark", "light"),
-  avatarURL: Joi.string(),
-  path: Joi.binary(),
+  username: Joi.string().min(4).pattern().empty(false).messages({
+    "string.base": "The username must be a string.",
+    "string.empty": "The username must not be empty.",
+  }),
+  birthday: Joi.string().pattern(datePattern).empty(false).messages({
+    "string.base": "The birthday must be a string.",
+    "string.empty": "The birthday must not be empty.",
+    "string.pattern.base":
+      "The email must be in format 2004-02-11(Year-Month-Day)",
+  }),
+  email: Joi.string().pattern(emailPattern).empty(false).messages({
+    "string.base": "The email must be a string.",
+    "string.empty": "The email must not be empty.",
+    "string.pattern.base": "The email must be in format test@gmail.com.",
+  }),
+  phone: Joi.string().pattern(phonePattern).empty(false).messages({
+    "string.base": "The phone must be a string.",
+    "string.empty": "The phone must not be empty.",
+    "string.pattern.base": "The email must be in format +380111111111",
+  }),
+  skype: Joi.string().pattern(phonePattern).empty(false).messages({
+    "string.base": "The skype must be a string.",
+    "string.empty": "The skype must not be empty.",
+    "string.pattern.base": "The email must be in format +380111111111",
+  }),
+  theme: Joi.string().valid(themeList).empty(false).messages({
+    "string.base": "The theme must be a string.",
+    "string.empty": "The theme must not be empty.",
+    "string.valid": "The theme must be either 'dark' or 'light'.",
+  }),
+  avatarURL: Joi.string().empty(false).messages({
+    "string.base": "The avatarURL must be a string.",
+    "string.empty": "The avatarURL must not be empty.",
+  }),
 });
 
 export const userVerifySchema = Joi.object({
-  email: Joi.string()
-    .pattern(emailPattern)
-    .required()
-    .messages({ message: "missing required field email" }),
+  email: Joi.string().pattern(emailPattern).required().empty(false).messages({
+    "string.base": "The email must be a string.",
+    "any.required": "The email field is required.",
+    "string.empty": "The email must not be empty.",
+    "string.pattern.base": "The email must be in format test@gmail.com.",
+  }),
 });
 
 export default User;
