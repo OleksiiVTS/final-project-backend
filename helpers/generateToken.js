a0e07dacde35b38bc4781725ba3e6cd6e9babd39
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-10-20 16:55:31 +0300

    Revert "Fix/review"

--- helpers/generateToken.js
+++ helpers/generateToken.js
@@ -2,8 +2,14 @@ import jwt from "jsonwebtoken";
 
 const { JWT_SECRET } = process.env;
 
-const generateToken = (payload) => {
+const generateToken = async (user) => {
+  const { _id: id } = user;
+  const payload = { id };
   const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+
+  user.token = token;
+
+  await user.save();
 
   return token;
 };
--- middleware/validation/authenticate.js
+++ middleware/validation/authenticate.js
@@ -15,10 +15,7 @@ const authenticate = async (req, res, next) => {
   }
   try {
     const { id } = jwt.verify(token, JWT_SECRET);
-    const user = await User.findById(id).populate(
-      "ownReview",
-      "comment rating _id"
-    );
+    const user = await User.findById(id);
     if (!user || user.token === "") {
       throw HttpError(401, "Not authorized");
     }
